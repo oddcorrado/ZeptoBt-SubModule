836f7a550105fdf8ca05a04114445e01bce56fbf
oddcorrado <corradolongoni@gmail.com>
2023-02-19 15:27:11 +0100

    perturbations viewer

--- Scripts/UI/ZeptoBtViewNode.cs
+++ Scripts/UI/ZeptoBtViewNode.cs
@@ -12,6 +12,10 @@ public class ZeptoBtViewNode : MonoBehaviour
     [SerializeField] TMP_Text commentsDisplay;
     [SerializeField] SpriteRenderer spriteRenderer;
     [SerializeField] Image image;
+    [SerializeField] Image imageSuccessPerturbation;
+    [SerializeField] Image imageFailPerturbation;
+    [SerializeField] Image imageRunPerturbation;
+    [SerializeField] Image imageUnprocessedPerturbation;
     [SerializeField] Image focusImage;
     [SerializeField] Image connectorTop;
     [SerializeField] Image connectorBot;
@@ -19,6 +23,11 @@ public class ZeptoBtViewNode : MonoBehaviour
     [SerializeField] Color failColor = new Color(0.7f, 0, 0, 1);
     [SerializeField] Color runColor = new Color(0, 0.5f, 0.5f, 1);
     [SerializeField] Color unprocessedColor = new Color(0.5f, 0.5f, 0.5f, 1);
+
+    float latestSuccessDate;
+    float latestFailDate;
+    float latestUnprocessedDate;
+    float latestRunDate;
 
     Color targetColor;
 
@@ -70,11 +79,23 @@ public class ZeptoBtViewNode : MonoBehaviour
                 NodeReturn.Runnning => runColor,
                 _ => unprocessedColor
             };
+
+            switch (value)
+            {
+                case NodeReturn.Success: latestSuccessDate = Time.time; break;
+                case NodeReturn.Failure: latestFailDate = Time.time; break;
+                case NodeReturn.Runnning: latestRunDate = Time.time; break;
+                case NodeReturn.Unprocessed: latestUnprocessedDate = Time.time; break;
+            } 
         }
     }
 
     void Update()
     {
         image.color = targetColor; // (1 - Time.deltaTime) * image.color + Time.deltaTime * targetColor;
+        imageFailPerturbation.color = Mathf.Max(0, 1 - 0.2f * (Time.time - latestFailDate)) * failColor;
+        imageSuccessPerturbation.color = Mathf.Max(0, 1 - 0.2f * (Time.time - latestSuccessDate)) * successColor;
+        imageRunPerturbation.color = Mathf.Max(0, 1 - 0.2f *  (Time.time - latestRunDate)) * runColor;
+        imageUnprocessedPerturbation.color = Mathf.Max(0, 1 - 0.2f * (Time.time - latestUnprocessedDate)) * unprocessedColor;
     }
 }
